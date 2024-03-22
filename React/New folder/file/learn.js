ba3b67c6a2b1f13b257800faeff7953ea771e019
VISHAL KUMAR <vishalkumarkumar3472@gmail.com>
2024-03-22 12:45:26 +0530

    update learn.js

--- React/New folder/file/learn.js
+++ React/New folder/file/learn.js
@@ -1,7 +1,29 @@
-let  arr1=[1,2,3,4,5]
+// let  arr1=[21,12,13,4,5]
 
-// function double(item){
-//     return item%2==0;
+// // function double(item){
+// //     return item%2==0;
+// // }
+// // const output=arr1.filter((item)=>item%2==0)
+// // console.log(output)
+
+// let r=arr1.reduce((acc,cur)=>{
+
+//     // return acc+=cur;
+//     if(cur>acc){
+//         acc=cur;
+//     }
+//     return acc;
+
 // }
-const output=arr1.filter((item)=>item%2==0)
-console.log(output)
+
+// ,0 )
+// console.log(r)
+
+let arr1=[1,2,3,4,6];
+function abc(x,y,...z){
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    // console.log(a+b)
+}
+abc(...arr1)
